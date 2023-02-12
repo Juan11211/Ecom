deb8e5e2db881d1f43a1522e0599a7c0b9459ed4
Juan Aviles <avilesjuan628@gmail.com>
2023-02-12 01:10:44 -0500

    working on productId page

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;`
+
+const ImageContainer = styled.div`
+    flex: 1; ` 
+
+const Title = styled.h1`
+font-weight: 300;`
+
+const Desc = styled.p`
+  margin: 20px 0px`
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px; 
+  `
+
+const Image = styled.img`
+  width: 100%; 
+  height: 90vh;
+  object-fit: cover;`
+
+const InfoContainer = styled.div`
+flex: 1;
+padding: 0px 50px;
+`
 
 function Product() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImageContainer>
+          <Image src="https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png" />
+        </ImageContainer>
+        <InfoContainer>
+          <Title>CODE COFFEE</Title>
+          <Desc>this is from the latest drop.</Desc>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
   )
 }
 
--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -1,10 +1,77 @@
+
 import React from 'react'
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Product from '../components/Product'
+import Products from '../components/Products'
+
+const Container = styled.div` 
+  margin: 20px;`
+const Title = styled.h2` `
+const FilterContainer = styled.div` 
+  display: flex;
+  justify-content: space-between;`
+const Filter = styled.div`
+  margin: 20px;`
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;`
+
+const Select = styled.select`
+  margin-right: 20px;
+  padding: 10px;
+`
+
+const Option = styled.option` `
 
 function ProductList() {
   return (
-    <div>
-      
-    </div>
+    <Container> 
+      <Announcement /> 
+      <Navbar />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter><FilterText>Filter Products</FilterText>
+        <Select>
+        <Option disabled selected>
+          Color
+        </Option>
+        <Option>White</Option>
+        <Option>Black</Option>
+        <Option>Red</Option>
+        <Option>Blue</Option>
+        <Option>Green</Option>
+        </Select>
+
+        <Select>
+        <Option disabled selected>
+          Size
+        </Option>
+        <Option>XS</Option>
+        <Option>S</Option>
+        <Option>M</Option>
+        <Option>L</Option>
+        <Option>XL</Option>
+        </Select>
+        </Filter>
+      <Filter><FilterText>Sort Products</FilterText>
+        <Select>
+          <Option selected>Newest</Option>
+          <Option>Highest</Option>
+          <Option>Lowest</Option>
+        </Select>
+      </Filter> 
+      </FilterContainer>
+      <Products/>
+      <Newsletter />
+      <Footer />
+     
+     
+    </Container>
   )
 }
 

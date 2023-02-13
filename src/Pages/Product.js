40430436dc250e18278e468e8b971be472ab38e5
Juan Aviles <avilesjuan628@gmail.com>
2023-02-13 10:18:51 -0500

    more update, need to finish

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
@@ -34,6 +35,48 @@ const InfoContainer = styled.div`
 flex: 1;
 padding: 0px 50px;
 `
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+  `
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`
+
+const FilterSizeOptions = styled.option``
+
+const FilterColor = styled.div`
+   width: 20px;
+   height: 20px;
+   border-radius: 50%;
+   background-color: ${props => props.color};
+   margin: 0px 5px;`
+
+const FilterContainer = styled.div`
+    display: flex;
+    width: 50%;
+    margin: 30px 0px;
+    justify-content: space-between;`
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;`
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;`
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;`
+
+const Amount = styled.span``
+
+const Button = styled.button``
 
 function Product() {
   return (
@@ -47,6 +90,33 @@ function Product() {
         <InfoContainer>
           <Title>CODE COFFEE</Title>
           <Desc>this is from the latest drop.</Desc>
+          <Price>12.99</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+                <FilterColor color='black' />
+                <FilterColor color='darkblue' />
+                <FilterColor color='gray'/>
+            </Filter>
+            <Filter>
+              <FilterTitle>Size: </FilterTitle>
+              <FilterSize>
+                <FilterSizeOptions>XS</FilterSizeOptions>
+                <FilterSizeOptions>S</FilterSizeOptions>
+                <FilterSizeOptions>M</FilterSizeOptions>
+                <FilterSizeOptions>L</FilterSizeOptions>
+                <FilterSizeOptions>XL</FilterSizeOptions>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer></AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+         
+          <Button>ADD TO CART</Button> 
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />

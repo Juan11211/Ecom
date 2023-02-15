dd97eee94569ffd6988b4e8feaf5b6beea11517a
Juan11211 <avilesjuan628@gmail.com>
2023-02-15 17:52:10 -0500

    almost done, making it responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,12 @@ import { Search, ShoppingCartCheckoutOutlined } from "@mui/icons-material";
 import { Badge } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
-
+import {mobile} from '../responsive'
 const Container = styled.div`
   height: 60px;
+  ${mobile({
+    height: "50px"
+  })}
 `;
 
 const Wrapper = styled.div`
@@ -13,6 +16,9 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({
+    padding: "10px 0px"
+  })}
 `;
 
 const Left = styled.div`
@@ -24,6 +30,9 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({
+    display: "none"
+  })}
 `;
 
 const SearchContainer = styled.div`
@@ -36,6 +45,9 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({
+    width: "50px"
+  })}
 `;
 
 const Center = styled.div`
@@ -45,6 +57,9 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({
+    fontSize: "24px"
+  })}
   
 `;
 const Right = styled.div`
@@ -52,6 +67,9 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({
+    justifyContent: "center"
+  })}
   
 `;
 
@@ -59,6 +77,10 @@ const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({
+    fontSize: "12px",
+    marginLeft: "10px"
+  })}
   
 `;
 
